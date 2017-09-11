3aeb94d8cf87e9d44041f4683d6d20e19b44975e
freignat91 <freignat@axway.com>
2017-09-11 15:54:55 +0200

    fix startup issue

--- tests/launchAmpbeatService.sh
+++ tests/launchAmpbeatService.sh
@@ -1,6 +1,5 @@
-docker service create --with-registry-auth --network ampcore_infra --name dbeat \
-    --label io.amp.role="infrastructure" \
+docker service create --network aNetwork --name dbeat \
     --mode global \
     --mount type=volume,source=dbeat,target=/containers \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-    Axway/elasticsearch-docker-beat
+    Axway/elasticsearch-docker-beat:latest
