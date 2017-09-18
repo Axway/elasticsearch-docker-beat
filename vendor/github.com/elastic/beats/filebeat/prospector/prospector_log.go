6eba7773e9ae1e6fcc0fb7fdf17c45b0688f4441
freignat91 <freignat@axway.com>
2017-09-18 10:11:30 +0200

    upgrade beat to version 5.6

--- vendor/github.com/elastic/beats/filebeat/prospector/prospector_log.go
+++ vendor/github.com/elastic/beats/filebeat/prospector/prospector_log.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
-	"strings"
 	"time"
 
 	"github.com/elastic/beats/filebeat/harvester"
@@ -30,6 +28,10 @@ func NewProspectorLog(p *Prospector) (*ProspectorLog, error) {
 	prospectorer := &ProspectorLog{
 		Prospector: p,
 		config:     p.config,
+	}
+
+	if len(p.config.Paths) == 0 {
+		return nil, fmt.Errorf("each prospector must have at least one path defined")
 	}
 
 	return prospectorer, nil
@@ -180,13 +182,14 @@ func (p *ProspectorLog) getFiles() map[string]os.FileInfo {
 
 // matchesFile returns true in case the given filePath is part of this prospector, means matches its glob patterns
 func (p *ProspectorLog) matchesFile(filePath string) bool {
+
+	// Path is cleaned to ensure we always compare clean paths
+	filePath = filepath.Clean(filePath)
+
 	for _, glob := range p.config.Paths {
 
-		if runtime.GOOS == "windows" {
-			// Windows allows / slashes which makes glob patterns with / work
-			// But for match we need paths with \ as only file names are compared and no lookup happens
-			glob = strings.Replace(glob, "/", "\\", -1)
-		}
+		// Glob is cleaned to ensure we always compare clean paths
+		glob = filepath.Clean(glob)
 
 		// Evaluate if glob matches filePath
 		match, err := filepath.Match(glob, filePath)
