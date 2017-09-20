c1fc1ca98b706d538760e45695e734ba6d062f55
Ulysses Souza <usouza@axway.com>
2017-09-20 17:37:30 +0200

    Add shebang and possibility to receive parameters on start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
+#!/bin/ash
 cd /etc/dbeat
 echo starting conffile updater
 ./updater
 cat /etc/beatconf/dbeat.yml
 echo starting dbeat
-./dbeat -e -c /etc/beatconf/dbeat.yml
+./dbeat -e -c /etc/beatconf/dbeat.yml $@
